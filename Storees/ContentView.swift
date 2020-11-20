a79bd5adcdd72f1e4e0fa11cff0caf197a3f1b99
joogps <joogps@gmail.com>
2020-11-20 12:23:46 -0300

    Initial Commit

---
+++ Storees/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Storees
+//
+//  Created by João Gabriel Pozzobon dos Santos on 20/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Storees/StoreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreesApp.swift
+//  Storees
+//
+//  Created by João Gabriel Pozzobon dos Santos on 20/11/20.
+//
+
+import SwiftUI
+
+@main
+struct StoreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
